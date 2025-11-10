c697d51bf3a65ecc123e60e5610ad145b8763943
TJ Miller <travismiller.design@gmail.com>
2025-11-09 19:18:00 -0500

    init frontend

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
